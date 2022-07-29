6326cad8ea9e87c3303e31792eb1a31786c70100
JavohirXoldorov <javohirxoldorov7@gmail.com>
2022-07-29 07:33:47 +0500

    testing

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a, b;
-        double result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2.0);
+        int a = 2, b = 5;
+        double result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
         System.out.println(result);
     }
 }
